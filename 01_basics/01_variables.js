e6f90d2845797d37a4963a2ed30d3dc377621b08
mubashirislam20000 <mubashirislam20000@gmail.com>
2026-05-13 14:14:13 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453 // not changed
+let accountEmail = "md@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+let accountStates;
+
+// accountId = "msd@gmail.com"
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountStates])
+
+
+
+
+// prefer not to use var becz of issue in block scope and functional scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all ur js code as newer version
+// alert("hello")
+
+console.log(3+3)
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+ 
+//// primitive data types
+// number = 2 to the power 53
+//  bigint
+// string = ""
+// boolean = true/false
+// null -> standalone value
+// underfined
+// symbol
+
+
+//// object
+
+
+
+
+console.log(typeof 143)
+console.log(typeof "hello")
+console.log()
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined

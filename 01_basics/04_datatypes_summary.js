5006681ae636940285415b8e6678453485806707
mubashirislam20000 <mubashirislam20000@gmail.com>
2026-05-14 08:52:37 +0000

    adding info about memory in datatypes summary file

--- 01_basics/04_datatypes_summary.js
+++ 01_basics/04_datatypes_summary.js
@@ -1,35 +1,61 @@
-// const id = Symbol("123")
-// const aid = Symbol("123")
-// console.log(id)
-// console.log(typeof aid)
-// console.log(id === aid)
+// // const id = Symbol("123")
+// // const aid = Symbol("123")
+// // console.log(id)
+// // console.log(typeof aid)
+// // console.log(id === aid)
 
-// const bigNum = 9343543354684638466n  // bigint
-// console.log(typeof bigNum)
+// // const bigNum = 9343543354684638466n  // bigint
+// // console.log(typeof bigNum)
 
 
-// reference (non primitive)
-// array, objects , functions
+// // reference (non primitive)
+// // array, objects , functions
 
-const heros = ["spiderman","hulk","Thor"]
-console.log(heros)
-console.log(typeof heros) 
+// const heros = ["spiderman","hulk","Thor"]
+// console.log(heros)
+// console.log(typeof heros) 
 
-let myObj = {
-    name:"md",
-    age:25,
+// let myObj = {
+//     name:"md",
+//     age:25,
+// }
+// console.log(myObj) 
+// console.log(typeof myObj) 
+
+
+
+
+// const myFunction = function(){
+//     console.log("Hello World")
+
+// }
+// console.log(typeof xyz)
+// console.log(typeof null)
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/* ****************Memory******************************************* */
+
+// Stack( Primitive) ,  Heap(Non-Primitive )
+
+let myYoutube = "hiteshchoudharydotcom"
+let anotherYoutube = myYoutube
+anotherYoutube = "chaiaurcode"
+
+console.log(myYoutube)
+console.log(anotherYoutube)
+
+
+let userOne = {
+    email : "srk@gmail.com",
+    upi : "srk@ybl"
+
 }
-console.log(myObj) 
-console.log(typeof myObj) 
+let userTwo = userOne
 
+console.table([userOne,userTwo])
 
-
-
-const myFunction = function(){
-    console.log("Hello World")
-
-}
-console.log(typeof xyz)
-console.log(typeof null)
-console.log(typeof myObj)
-console.log(typeof myFunction)
+userTwo.email= "srkhan@gmai.com"
+console.table([userOne,userTwo])

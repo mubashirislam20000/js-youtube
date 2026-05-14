7f70f3cf4f828844883c7012aa93ff580be2f332
mubashirislam20000 <mubashirislam20000@gmail.com>
2026-05-14 06:50:44 +0000

    conversion operators

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+// let score = undefined;
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// console.log()
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log((valueInNumber)) // NaN -> Not A NUMBER 
+
+// // "33" -> 33
+// // "33abc" -> NaN
+// // null -> 0
+// // undefine -> NaN
+// // true -> 1 false -> 0
+
+// let isLoggedIn = 1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)  
+
+
+// ***********Opertaions******************
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 =  "harry"
+// let str3 =  str1 + " " +str2
+// console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+console.log(+true)
+console.log(+true)
+
+let num1,num2,num3
+num1 = num2 = num3 = 4
+console.table([num1,num2,num3])
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)

a5587b85d29b892d84703bfb4f5160cc65225fcd
mubashirislam20000 <mubashirislam20000@gmail.com>
2026-05-14 07:47:14 +0000

    datatypes summary

---
+++ 01_basics/04_datatypes_summary.js
@@ -0,0 +1,35 @@
+// const id = Symbol("123")
+// const aid = Symbol("123")
+// console.log(id)
+// console.log(typeof aid)
+// console.log(id === aid)
+
+// const bigNum = 9343543354684638466n  // bigint
+// console.log(typeof bigNum)
+
+
+// reference (non primitive)
+// array, objects , functions
+
+const heros = ["spiderman","hulk","Thor"]
+console.log(heros)
+console.log(typeof heros) 
+
+let myObj = {
+    name:"md",
+    age:25,
+}
+console.log(myObj) 
+console.log(typeof myObj) 
+
+
+
+
+const myFunction = function(){
+    console.log("Hello World")
+
+}
+console.log(typeof xyz)
+console.log(typeof null)
+console.log(typeof myObj)
+console.log(typeof myFunction)

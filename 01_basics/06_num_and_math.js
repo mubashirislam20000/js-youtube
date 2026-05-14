656eaa68ff19791686a41d72dbaea06b8c6cdf17
mubashirislam20000 <mubashirislam20000@gmail.com>
2026-05-14 10:39:36 +0000

    math and num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,30 @@
+// const score = 400
+// const score1 = String(400)
+
+// const balance = new Number(100)
+
+// console.log(score)
+// console.log(score1)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// console.log(123.4.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+
+//************************ Maths ****************************//
+
+// console.log(Math)
+// console.log(Math.PI)
+// console.log(Math.abs(-545))
+// console.log(Math.round(54.5))
+// console.log(Math.max(10,15,14,16,20))
+
+console.log(Math.random()*10) // 0 - 1
+
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
